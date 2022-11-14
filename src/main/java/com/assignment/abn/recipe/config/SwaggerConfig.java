30a0acb2a35f2cbc057709c774cb3fa0e9da39d4
indrajitbastapure@gmail.com <indrajitbastapure@gmail.com>
2022-11-14 07:00:22 +0100

    Update swagger config

--- src/main/java/com/assignment/abn/recipe/config/SwaggerConfig.java
+++ src/main/java/com/assignment/abn/recipe/config/SwaggerConfig.java
@@ -17,26 +17,16 @@ import java.util.Collections;
 @EnableSwagger2
 public class SwaggerConfig {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.assignment.abn.recipe.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false).select()
+				.apis(RequestHandlerSelectors.basePackage("com.assignment.abn.recipe"))
+				.paths(PathSelectors.any()).build().apiInfo(apiInfo());
+	}
 
-    private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "Recipe Manager Application Service",
-                "Documentation of the Recipe Manager Application Service.",
-                "0.0.1",
-                "",
-                ApiInfo.DEFAULT_CONTACT,
-                "",
-                "",
-                Collections.emptyList());
-    }
+	private ApiInfo apiInfo() {
+		return new ApiInfo("Recipe Manager Application Service",
+				"Documentation of the Recipe Manager Application Service.", "0.0.1", "", ApiInfo.DEFAULT_CONTACT, "",
+				"", Collections.emptyList());
+	}
 }
